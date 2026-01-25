9e7a358c9e4928eb9b4046f3194364dd0f972df8
Blue Velvet <BM.VelvetSociety@protonmail.com>
2026-01-25 21:30:02 +0000

    Feature/UI tests (#1)
    * Update AuthenticateHytaleCommand.cs
    
    * Update AuthenticateHytaleCommand.cs
    
    Updated Command to showcase better information to user
    
    * Added a command to open mod project suing antigravity, need to add other ide options in future
    
    * New Commands+Changes
    
    Added new commands to launch and build mods + server
    
    * Updated Project File
    
    * Removed Sample Test Commands
    
    //These commands were made when first testing the CLI, they shouldn't be part of the build
    
    * Inteligent comand completions
    
    Added Inteligent command completions when typing
    
    * Update InputPalette.cs
    
    Updated Input Pallete so the cursor is now no longer flashing when typing aggresively
    
    * ReadMe + Images
    
    Updated commands, now called Auth and Server
    Added Images of the CLI
    
    * Update README.md

--- Commands/ExitCommand.cs
+++ Commands/ExitCommand.cs
@@ -1,3 +1,5 @@
+using Spectre.Console;
+
 namespace VelvetCLI.Commands;
 
 internal sealed class ExitCommand : VelvetCommand
@@ -6,8 +8,14 @@ internal sealed class ExitCommand : VelvetCommand
     public override string Description => "Exit the application";
     public override bool ShouldExitAfterRun => true;
 
-    protected override void ExecuteCore()
+    protected override void ExecuteCore(string[] args)
     {
         Console.WriteLine("\nExiting...");
+    }
+    public override void ShowHelp()
+    {
+        base.ShowHelp();
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupLine("[bold yellow]Usage:[/] exit");
     }
 }
--- Commands/InitialiseWorkspaceCommand.cs
+++ Commands/InitialiseWorkspaceCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Spectre.Console;
 
 namespace VelvetCLI.Commands
 {
@@ -8,7 +9,7 @@ namespace VelvetCLI.Commands
         public override string Name => "init";
         public override string Description => "Initialise MODS workspace";
 
-        protected override void ExecuteCore()
+        protected override void ExecuteCore(string[] args)
         {
             const string modsFolder = "mods";
 
@@ -28,6 +29,14 @@ namespace VelvetCLI.Commands
             {
                 Console.WriteLine("Error during initialisation: " + ex.Message);
             }
+        }
+        public override void ShowHelp()
+        {
+            base.ShowHelp();
+            AnsiConsole.WriteLine();
+            AnsiConsole.MarkupLine("[bold yellow]Usage:[/] init");
+            AnsiConsole.WriteLine();
+            AnsiConsole.MarkupLine("[grey]Note: This command creates the 'mods' folder if it doesn't already exist, initializing the current directory as a Velvet workspace.[/]");
         }
     }
 }
